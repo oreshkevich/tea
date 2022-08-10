42e8b5adae1fc9f19858b0235847e32df46994eb
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-08-10 18:28:59 +0300

    fix: js

--- js/search.js
+++ js/search.js
@@ -1,18 +1,26 @@
 function searchInput() {
-  let input = document.getElementById('search-input');
+  let input = document.getElementById('search-weight');
   function search() {
     let filter = input.value.toUpperCase();
+    let FilterSearchEmpty = document.querySelector('.js-empty-weight');
     let list = document.getElementById('list');
     let jsSearchField = list.querySelectorAll(
       '.directory-content__filter-field'
     );
+    let count = 0;
     for (let i = 0; i < jsSearchField.length; i++) {
       let label = jsSearchField[i].getElementsByTagName('label')[0];
       if (label.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        count += 1;
         jsSearchField[i].style.display = '';
       } else {
         jsSearchField[i].style.display = 'none';
       }
+    }
+    if (!count) {
+      FilterSearchEmpty.style.display = 'block';
+    } else {
+      FilterSearchEmpty.style.display = 'none';
     }
   }
   document.addEventListener('keyup', search);
@@ -29,17 +37,25 @@ function searchBrand() {
   let searchInput = document.getElementById('search-brand');
   function search() {
     let filter = searchInput.value.toUpperCase();
+    let FilterSearchEmpty = document.querySelector('.js-empty-brand');
     let list = document.getElementById('list-brand');
     let jsSearchField = list.querySelectorAll(
       '.directory-content__filter-field'
     );
+    let count = 0;
     for (let i = 0; i < jsSearchField.length; i++) {
       let label = jsSearchField[i].getElementsByTagName('label')[0];
       if (label.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        count += 1;
         jsSearchField[i].style.display = '';
       } else {
         jsSearchField[i].style.display = 'none';
       }
+    }
+    if (!count) {
+      FilterSearchEmpty.style.display = 'block';
+    } else {
+      FilterSearchEmpty.style.display = 'none';
     }
   }
   document.addEventListener('keyup', search);
@@ -56,17 +72,25 @@ function searchCountry() {
   let searchInput = document.getElementById('search-country');
   function search() {
     let filter = searchInput.value.toUpperCase();
+    let FilterSearchEmpty = document.querySelector('.js-empty-country');
     let list = document.getElementById('list-country');
     let jsSearchField = list.querySelectorAll(
       '.directory-content__filter-field'
     );
+    let count = 0;
     for (let i = 0; i < jsSearchField.length; i++) {
       let label = jsSearchField[i].getElementsByTagName('label')[0];
       if (label.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        count += 1;
         jsSearchField[i].style.display = '';
       } else {
         jsSearchField[i].style.display = 'none';
       }
+    }
+    if (!count) {
+      FilterSearchEmpty.style.display = 'block';
+    } else {
+      FilterSearchEmpty.style.display = 'none';
     }
   }
   document.addEventListener('keyup', search);
@@ -83,17 +107,26 @@ function searchIngredients() {
   let searchInput = document.getElementById('search-ingredients');
   function search() {
     let filter = searchInput.value.toUpperCase();
+    let FilterSearchEmpty = document.querySelector('.js-empty-ingredients');
     let list = document.getElementById('list-ingredients');
     let jsSearchField = list.querySelectorAll(
       '.directory-content__filter-field'
     );
+    let count = 0;
     for (let i = 0; i < jsSearchField.length; i++) {
       let label = jsSearchField[i].getElementsByTagName('label')[0];
       if (label.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        count += 1;
         jsSearchField[i].style.display = '';
       } else {
         jsSearchField[i].style.display = 'none';
       }
+    }
+
+    if (!count) {
+      FilterSearchEmpty.style.display = 'block';
+    } else {
+      FilterSearchEmpty.style.display = 'none';
     }
   }
   document.addEventListener('keyup', search);
