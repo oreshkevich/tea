09c3321fff80a0e0b23a8ed93444138f8b3f3de9
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-09-01 18:10:01 +0300

    fix: input

---
+++ js/file.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const forms = document.querySelectorAll('form');
+  const inputFile = document.querySelectorAll('.upload-file__input');
+
+  /////////// Кнопка «Прикрепить файл» ///////////
+  inputFile.forEach(function (el) {
+    let textSelector = document.querySelector('.upload-file__text');
+    let fileList;
+
+    // Событие выбора файла(ов)
+    el.addEventListener('change', function (e) {
+      // создаём массив файлов
+      fileList = [];
+      for (let i = 0; i < el.files.length; i++) {
+        fileList.push(el.files[i]);
+      }
+
+      // вызов функции для каждого файла
+      fileList.forEach((file) => {
+        uploadFile(file);
+      });
+    });
+
+    // Проверяем размер файлов и выводим название
+    const uploadFile = (file) => {
+      // файла <5 Мб
+      if (file.size > 5 * 1024 * 1024) {
+        alert('Файл должен быть не более 5 МБ.');
+        return;
+      }
+
+      // Показ загружаемых файлов
+      if (file && file.length > 1) {
+        if (file.length <= 4) {
+          textSelector.textContent = `Выбрано ${file.length} файла`;
+        }
+        if (file.length > 4) {
+          textSelector.textContent = `Выбрано ${file.length} файлов`;
+        }
+      } else {
+        textSelector.textContent = file.name;
+      }
+    };
+  });
+});
