a7872eef34f1bbe35b8ae2dacd586e76710a7c0c
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-09-02 14:28:59 +0300

    add button

---
+++ js/modal-filter.js
@@ -0,0 +1,16 @@
+function modal() {
+  const modalBtn = document.querySelector('.more');
+  const modal = document.querySelector('.modal');
+
+  modalBtn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+  });
+
+  modal.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('modal__close')) {
+      modal.classList.add('hidden');
+    }
+  });
+}
+modal();
