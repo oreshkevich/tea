5101ec2304e63c740f6661fdb0454ee4d53926c8
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-09-02 16:05:24 +0300

    fix main.js

--- js/main.js
+++ js/main.js
@@ -7,45 +7,67 @@ function order() {
     '.order-delivery-courier'
   );
   const postamates = document.querySelector('.postamates');
-
   const ticketsCard = document.querySelector('.order-form__card');
-
   ticketsCard.addEventListener('click', (event) => {
-    let target = event.target;
-    console.log(event.target);
-    console.log(event.target.dataset.point);
-    console.log(event.target.getAttribute('data-point'));
-    if (target.value === 'courier') {
-      orderCourier.classList.add('delivery-item--active');
-      orderPostamat.classList.remove('delivery-item--active');
-      orderPickup.classList.remove('delivery-item--active');
+    //   let target = event.target;
+    //   console.log(event.currentTarget);
+    //   console.log(event.target.dataset.point);
+    //   console.log(event.target.getAttribute('data-point'));
+    //   if (target.value === 'courier') {
+    //     orderCourier.classList.add('delivery-item--active');
+    //     orderPostamat.classList.remove('delivery-item--active');
+    //     orderPickup.classList.remove('delivery-item--active');
+    //     deliveryAddress.classList.add('hide');
+    //     orderDeliveryCourier.classList.remove('hide');
+    //   }
+    //   if (target.value === 'postamat') {
+    //     orderPostamat.classList.add('delivery-item--active');
+    //     orderCourier.classList.remove('delivery-item--active');
+    //     orderPickup.classList.remove('delivery-item--active');
+    //     postamates.classList.remove('hide');
+    //     deliveryAddress.classList.add('hide');
+    //     orderDeliveryCourier.classList.add('hide');
+    //   }
+    if (event.target.dataset.point === 'point') {
+      console.log('hi');
       deliveryAddress.classList.add('hide');
       orderDeliveryCourier.classList.remove('hide');
-    }
-    if (target.value === 'postamat') {
-      orderPostamat.classList.add('delivery-item--active');
-      orderCourier.classList.remove('delivery-item--active');
-      orderPickup.classList.remove('delivery-item--active');
-      postamates.classList.remove('hide');
-      deliveryAddress.classList.add('hide');
-      orderDeliveryCourier.classList.add('hide');
-    }
-    if (event.target.dataset.point === 'point') {
-      orderPostamat.classList.add('delivery-item--active');
-      orderCourier.classList.remove('delivery-item--active');
-      orderPickup.classList.remove('delivery-item--active');
-      postamates.classList.remove('hide');
-      deliveryAddress.classList.add('hide');
-      orderDeliveryCourier.classList.add('hide');
-    }
-    if (target.value === 'pickup') {
-      orderPickup.classList.add('delivery-item--active');
-      orderCourier.classList.remove('delivery-item--active');
-      orderPostamat.classList.remove('delivery-item--active');
-      postamates.classList.add('hide');
+    } else {
       deliveryAddress.classList.remove('hide');
       orderDeliveryCourier.classList.add('hide');
     }
+
+    // if (target.value === 'pickup') {
+
+    // orderPickup.classList.add('delivery-item--active');
+    // orderCourier.classList.remove('delivery-item--active');
+    // orderPostamat.classList.remove('delivery-item--active');
+    // postamates.classList.add('hide');
+    // deliveryAddress.classList.remove('hide');
+    // orderDeliveryCourier.classList.add('hide');
+    // }
   });
+
+  const deliveryItem = document.querySelectorAll('.delivery-item');
+  deliveryItem.forEach((n, i, a) => {
+    // item.addEventListener('click', (event) => {
+    //   console.log(event.target);
+    //   if (event.currentTarget) {
+    //     item.classList.add('delivery-item--active');
+    //   }
+    //   item.classList.remove('delivery-item--active');
+    // });
+    n.addEventListener('click', () => {
+      a.forEach((m) => {
+        m.classList.toggle('delivery-item--active', m === n);
+      });
+    });
+  });
+  // document.querySelectorAll('.side__nav > li')
+  // deliveryItem.forEach((n, i, a) => {
+  //   n.addEventListener('click', () =>
+  //     a.forEach((m) => m.classList.toggle('is-active', m === n))
+  //   );
+  // });
 }
-// order();
+order();
